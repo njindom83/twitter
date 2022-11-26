9b3fd01bc35634e7345fdf7aba2801a07ec25bf2
Nancy <banyongnancy@gmail.com>
2022-11-26 21:01:59 +0000

    conflict2 resolved

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-#this is a deploy scrip
 deploy java application
 deploy python apps
-
 deploy in the dev environment
-deployment5 master
-
-deploy in the dev environment 
+deployment5 master 
 deployment4
+deploy to staing env
+echo good job
+Tested and accepted
